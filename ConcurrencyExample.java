b30abbe5bd34c03e62eff4625fc3cb1c180a2bb5
yasertirsen <48992948+yasertirsen@users.noreply.github.com>
2019-09-18 22:50:23 +0100

    Update ConcurrencyExample.java

--- ConcurrencyExample.java
+++ ConcurrencyExample.java
@@ -1,33 +1,37 @@
 class Foo {
-    private boolean ex1, ex2;
+    private boolean ex1, ex2 =false;
     public Foo() {
        
     }
 
     public synchronized void first(Runnable printFirst) throws InterruptedException {
-        ex1= true;
+        
         // printFirst.run() outputs "first". Do not change or remove this line.
         printFirst.run();
+        ex1= true;
+        this.notifyAll();
     }
     
 
     public synchronized void second(Runnable printSecond) throws InterruptedException {
        while (!ex1){
-            wait();
+            this.wait();
         }
-        ex2 = true;
-        notifyAll();
+        
         // printSecond.run() outputs "second". Do not change or remove this line.
         printSecond.run(); 
+        this.ex2 = true;
+        this.notifyAll();
     }
 
     public synchronized  void third(Runnable printThird) throws InterruptedException {
         while (!ex2){
             wait();
         }
-        notifyAll();
+        
         // printThird.run() outputs "third". Do not change or remove this line.
         printThird.run();
+        this.notifyAll();
     }
     
     
